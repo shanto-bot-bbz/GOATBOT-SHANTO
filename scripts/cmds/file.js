421b021e8b907438a4e3f8b0183052e74be59e9f
shanto-bot-bbz <botmakenew@gmail.com>
2026-02-22 15:54:49 +0600

    Create file.js

---
+++ scripts/cmds/file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "givefile",
+    aliases: ["file"],
+    version: "1.0",
+    author: "FAHAD",
+    countDown: 5,
+    role: 0,
+    description: "extract file",
+    category: "owner",
+    guide: "{pn} Write a file name"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["61588594672255"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("─꯭𓆩𝆠꯭፝֟𝐀𝐑𝐄 𝐘𝐎𝐔 𝐂𝐑𝐀𝐙𝐘 𝐎𝐍𝐋𝐘 𝐒𝐇𝐀𝐍𝐓𝐎 𝐁𝐎𝐒𝐒 𝐏𝐄𝐑𝐌𝐈𝐒𝐒𝐈𝐎𝐍 𝐓𝐎 𝐔𝐒𝐄 𝐓𝐇𝐈𝐒 𝐂𝐌𝐃 𝐅𝐈𝐋𝐄𝆠꯭፝֟𓆪. ⚠️", event.threadID, event.messageID);
+    }
+
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("🔰 provide a file name!", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
