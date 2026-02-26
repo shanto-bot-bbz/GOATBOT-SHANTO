bcf4e55bccb2a55cd6f311aeaef459274d2722e7
shanto-bot-bbz <botmakenew@gmail.com>
2026-02-26 21:29:59 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "fork",
     aliases: ["repo", "link"],
     version: "1.0",
-    author: "Aphelion",
+    author: "shanto",
     countDown: 3,
     role: 0,
     longDescription: "Returns the link to the official, updated fork of the bot's repository.",
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   onStart: async function({ message }) {
-    const text = "https://github.com/ShifuX69/Prime-GoatBot.git";
+    const text = "𝗦𝗵𝗮𝗻𝘁𝗼𝗿 𝗳𝗼𝗿𝗸 𝗽𝗿𝗶𝘃𝗮𝘁𝗲 𝗮𝗸𝗮𝘀𝗵 𝗮𝗿 𝘁𝗮 𝗻𝗲🐱 - https://github.com/akashx404/GOAT-ADVANCED.git";
     
     message.reply(text);
   }
